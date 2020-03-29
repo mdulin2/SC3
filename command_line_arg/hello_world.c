b99c653dece59fcd0d4eadb770d28cd2ffdb531a
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2020-03-29 00:43:07 -0700

    Add printf challenge... Need to add challenge and solution files later. Finished the actual challenge at about 1am

---
+++ command_line_arg/hello_world.c
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Compile: gcc hello_world.c -m32 -o hello_world
+*/
+
+// Get reads the flag... ignore this!
+char* get_flag(){
+	char* string = malloc(0x40);	
+
+	FILE *fp = fopen("flag.txt", "r");
+	size_t len = 0;
+	
+	if(fp == NULL)
+		exit(1);
+	
+	getline(&string,&len,fp);
+
+	return string; 
+
+}
+
+
+// Take my string, into a new buffer and print it!
+int main(int argc, char* argv[]){
+
+	char* flag = get_flag();	
+	char flag_buf[40]; // Tmp buffer for the flag
+	char my_buf[40];   // Tmp buffer for the user input
+	
+	int my_len = strlen(argv[1]);
+	if(my_len > 39){
+		printf("Maximum lenth string is 39");
+	}
+
+	// Copy flag into local buffer
+	strncpy(flag_buf, flag, 40);
+		
+	// Copy arguement into buffer	
+	strncpy(my_buf,argv[1], my_len);		
+
+	// Print my string from the command line!	
+	printf(my_buf);	
+	
+	return 0;
+
+}
