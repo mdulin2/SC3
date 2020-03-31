c3f9c550ea550c8a1066044a2a0c4e88d2b27db8
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2020-03-30 21:01:08 -0700

    Made the signals source code much more readable for the students (S/O to Zach M. for the tip on that)

--- signals/fork.c
+++ signals/fork.c
@@ -24,35 +24,59 @@ void* print_flag() {
 
 }
 
+
+// The code for the CHILD process
+void* child_process(){
+	puts("Stopping child process...");
+	raise(SIGSTOP); // SIGSTOP = 19 - This is signal call 19
+	print_flag();
+	return;
+}
+
+
+// The code for the PARENT process
+void* parent_process(pid_t pid1){
+	pid_t input_pid; 
+	int signal_call;
+	
+	sleep(2); // Allow the child process to execute first
+	printf("Child process ID: %d\n", pid1);
+	
+	// Get the process ID
+	printf("Enter a process ID: ");
+	scanf("%d", &input_pid);
+	
+	// Get the signal to use
+	printf("Enter a signal call: ");
+	scanf("%d", &signal_call);	
+		
+	// Send the signal call	
+	kill(input_pid, signal_call);
+	return;	
+
+}
+
 int main(int argc, char* argv[]){
 	
 	pid_t pid1;
-	pid_t input_pid; 
-	int signal_call; 
 	
-	// Fork the program. Create two different processes
+	/*
+	Fork the program. Create two different processes
+	In C, when 'fork' is called, a NEW process is created. 
+	- The PID (process ID) returned from the call for the PARENT, will be the process id of the CHILD.
+	- The PID of the CHILD will always be ZERO!! (0)
+	*/
 	pid1 = fork();
-	if(pid1 == 0){ // child process
-		puts("Stopping child process...");
-		raise(19); // SIGSTOP 
-
-		print_flag();
-
+	
+	// 0: The child process!
+	if(pid1 == 0){ 
+		// Child process!!! 
+		child_process();
 	}
-	else { // Parent process
-		sleep(2); // Allow the child process to execute first
-		printf("Child process ID: %d\n", pid1);
-	
-		// Get the process ID
-		printf("Enter a process ID: ");
-		scanf("%d", &input_pid);
-	
-		// Get the signal to use
-		printf("Enter a signal call: ");
-		scanf("%d", &signal_call);	
-		
-		// Send the signal call	
-		kill(input_pid, signal_call);
+	else { 
+		// Parent process!!!!
+		parent_process(pid1);
 	}
-	
+
+	return 0;
 }
