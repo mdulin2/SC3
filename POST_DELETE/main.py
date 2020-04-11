5f86425d656aab8c9db1ab587a9e283bb9c5acd0
Your Name <you@example.com>
2020-04-11 11:42:12 -0700

    Added POST_DELETE challenge

---
+++ POST_DELETE/main.py
@@ -0,0 +1,27 @@
+'''
+In order to run this, install the following: 
+- python3 
+- flask 
+
+To run: 
+- `python3 main.py`
+'''
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+flag = "flg{HTTP_re3pon3e_c0de3!}"
+flg_1, flg_2 = flag[:int(len(flag)/2)], flag[int(len(flag)/2):] # Cut the string in half
+
+@app.route("/route1", methods=['POST'])
+def put():
+	return flg_1 + '\n'
+
+@app.route("/route2", methods=['DELETE'])
+def patch(): 
+	return flg_2 + '\n'
+
+if __name__ == "__main__":
+    app.run(debug=True)
