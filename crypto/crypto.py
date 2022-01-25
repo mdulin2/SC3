f1810580b3d4c78963bafffbb24ed99e6c312780
Joseph Riddle <joeriddles10@gmail.com>
2022-01-24 20:29:28 -0800

    Change OTP to use key phrase instead of RNG

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,5 +1,4 @@
 import argparse
-import random
 
 UPPER_A_ORD = ord("A")
 LOWER_A_ORD = ord("a")
@@ -23,7 +22,7 @@ def shift_lower(char: str, shift: int) -> str:
     return shifted_char
 
 
-def shift_char(char: str, shift: str) -> str:
+def shift_char(char: str, shift: int) -> str:
     """Shift a character."""
     if char.isupper():
         char = shift_upper(char, shift)
@@ -42,15 +41,37 @@ def caesar_cipher(phrase: str, shift: int) -> str:
     return encrypted_phrase
 
 
-def one_time_pad(phrase: str, seed: int) -> str:
-    """One-time-pad encryption using a RNG seed."""
-    random.seed(seed)
+def one_time_pad_encrypt(phrase: str, key: str) -> str:
+    """One-time-pad encryption using a key."""
+    if len(key) < len(phrase):
+        raise ValueError("Key must be at least as long as phrase for encoding a OTP.")
     encrypted_phrase = ""
-    for char in phrase:
-        shift = random.randint(0, 25)
-        char = shift_char(char, shift)
-        encrypted_phrase += char
+    for phrase_char, key_char in zip(phrase, key):
+        is_upper = phrase_char.isupper()
+        phrase_ord = ord(phrase_char.lower()) - LOWER_A_ORD
+        key_ord = ord(key_char.lower()) - LOWER_A_ORD
+        encrypted_ord = (phrase_ord + key_ord) % 26
+        encrypted_char = chr(encrypted_ord + LOWER_A_ORD)
+        encrypted_char = encrypted_char.upper() if is_upper else encrypted_char
+        encrypted_phrase += encrypted_char
     return encrypted_phrase
+
+
+def one_time_pad_decrypt(encrypted_phrase: str, key: str) -> str:
+    """One-time-pad decryption using a key."""
+    if len(key) < len(encrypted_phrase):
+        raise ValueError("Key must be at least as long as encrypted phrase for decoding a OTP.")
+    phrase = ""
+    for encrypted_phrase_char, key_char in zip(encrypted_phrase, key):
+        is_upper = encrypted_phrase_char.isupper()
+        encrypted_phrase_ord = ord(encrypted_phrase_char.lower()) - LOWER_A_ORD
+        key_ord = ord(key_char.lower()) - LOWER_A_ORD
+        phrase_ord = (encrypted_phrase_ord - key_ord) % 26
+        phrase_char = chr(phrase_ord + LOWER_A_ORD)
+        phrase_char = phrase_char.upper() if is_upper else phrase_char
+        phrase += phrase_char
+    return phrase
+
 
 
 if __name__ == "__main__":
@@ -63,7 +84,7 @@ if __name__ == "__main__":
 
     otp_parser: argparse.ArgumentParser = subparsers.add_parser("otp")
     otp_parser.add_argument("phrase", help="Phrase to encrypt.")
-    otp_parser.add_argument("--seed", "-S", type=int, help="Seed for the random number generator.", default=42)
+    otp_parser.add_argument("key", "-K", help="Key for the one-time-pad generator.", required=True)
 
     cli_args = parser.parse_args()
     match cli_args.cmd:
@@ -71,5 +92,5 @@ if __name__ == "__main__":
             encrypted_phrase = caesar_cipher(cli_args.phrase, cli_args.shift)
             print(f"{cli_args.phrase} -> {encrypted_phrase}")
         case "otp":
-            encrypted_phrase = one_time_pad(cli_args.phrase, cli_args.seed)
+            encrypted_phrase = one_time_pad_encrypt(cli_args.phrase, cli_args.key)
             print(f"{cli_args.phrase} -> {encrypted_phrase}")
--- crypto/crypto_test.py
+++ crypto/crypto_test.py
@@ -16,11 +16,34 @@ def test__caesar_cipher(phrase: str, shift: int, expected: str):
 
 
 @pytest.mark.parametrize(
-    "phrase, shift, expected",
+    "phrase, key, expected",
     (
-        ("zork", 0, "lmel"),
-        ("zork", 42, "trrh"),
+        ("hello", "xmckl", "eqnvz"),
+        ("HELLO", "XMCKL", "EQNVZ"),
     )
 )
-def test__one_time_pad(phrase: str, shift: int, expected: str):
-    assert expected == crypto.one_time_pad(phrase, shift)
+def test__one_time_pad_encrypt(phrase: str, key: str, expected: str):
+    assert expected == crypto.one_time_pad_encrypt(phrase, key)
+
+
+@pytest.mark.parametrize(
+    "encrypted_phrase, key, expected",
+    (
+        ("eqnvz", "xmckl", "hello"),
+        ("EQNVZ", "XMCKL", "HELLO"),
+    )
+)
+def test__one_time_pad_decrypt(encrypted_phrase: str, key: str, expected: str):
+    assert expected == crypto.one_time_pad_decrypt(encrypted_phrase, key)
+
+
+@pytest.mark.parametrize(
+    "phrase, key",
+    (
+        ("eqnvz", "xmckl"),
+        ("EQNVZ", "XMCKL"),
+    )
+)
+def test__one_time_pad(phrase: str, key: str):
+    assert phrase == crypto.one_time_pad_decrypt(crypto.one_time_pad_encrypt(phrase, key), key)
+
